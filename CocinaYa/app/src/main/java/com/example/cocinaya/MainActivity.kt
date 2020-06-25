aa81afddf4319a550af3ae2e0aa0a46683aa07bc
Antonio Osornio <a.osornio@outlook.com>
2020-06-24 20:56:32 -0500

    Se realiza actividad inicial con exito

--- CocinaYa/app/src/main/java/com/example/cocinaya/MainActivity.kt
+++ CocinaYa/app/src/main/java/com/example/cocinaya/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.cocinaya
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnInicar.setOnClickListener {
+            val pasarDatos = Intent (this,ActivityComencemos::class.java)
+            startActivity(pasarDatos)
+        }
     }
 }
