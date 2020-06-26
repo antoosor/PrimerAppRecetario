ed331fe3911db7ce9ed647d35a15a2305ce46ecc
Antonio Osornio <a.osornio@outlook.com>
2020-06-26 17:24:29 -0500

    Se agrega activity de la primera receta, al momento del cambio entre activitys no se visualizan prueblemas

--- CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
+++ CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
@@ -1,9 +1,10 @@
 package com.example.cocinaya
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.view.View
+import android.widget.*
 
 class IndiceDeSopas : AppCompatActivity() {
 
@@ -15,5 +16,20 @@ class IndiceDeSopas : AppCompatActivity() {
         val adapter = ArrayAdapter (this,R.layout.item_sopas,arregloSopas)
         val listView =  findViewById<ListView>(R.id.ListView)
         listView.adapter = adapter
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener{
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (position == 0){
+                    val paseAsopaAguada = Intent(
+                        this@IndiceDeSopas,RecetaSopaAguada::class.java)
+                        startActivity(paseAsopaAguada)
+
+                }
+                if (position == 1){
+                    Toast.makeText(this@IndiceDeSopas,"Sopa tortilla",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
     }
 }
