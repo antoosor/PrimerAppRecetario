7e1fa0c311e4a47b5ba7b3e44df1ddb542e5caf5
Antonio Osornio <a.osornio@outlook.com>
2020-06-26 00:25:50 -0500

    Se agrega listView al activity de indice de sopas de forma exitosa, se detecta que se debe de cambiar  el nombre de sopas a caldos

--- CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
+++ CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
@@ -2,11 +2,18 @@ package com.example.cocinaya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class IndiceDeSopas : AppCompatActivity() {
 
+    var arregloSopas = arrayOf("Sopa Aguada", "Sopa de Tortilla", "Caldo de Pollo","Caldo de res")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_indice_de_sopas)
+
+        val adapter = ArrayAdapter (this,R.layout.itemsopa,arregloSopas)
+        val listView =  findViewById<ListView>(R.id.ListView)
+        listView.adapter = adapter
     }
 }
