56bcb0dfb59be4e6cdb9aeff1d4b85b65537cd81
Antonio Osornio <a.osornio@outlook.com>
2020-06-26 13:56:11 -0500

    Se agrega listview de forma exitosa para el indice de entradas

--- CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
+++ CocinaYa/app/src/main/java/com/example/cocinaya/IndiceDeSopas.kt
@@ -7,12 +7,12 @@ import android.widget.ListView
 
 class IndiceDeSopas : AppCompatActivity() {
 
-    var arregloSopas = arrayOf("Sopa Aguada", "Sopa de Tortilla", "Caldo de Pollo","Caldo de res")
+    var arregloSopas = arrayOf("Sopa Aguada", "Sopa de Tortilla", "Caldo de Pollo","Caldo de res","Arroz  blanco","Arroz rojo","Arroz verde","Arroz chino","Crema champiñones","Crema de queso","Crema de Zanahoria","Crema de chile  Poblano")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_indice_de_sopas)
 
-        val adapter = ArrayAdapter (this,R.layout.itemsopa,arregloSopas)
+        val adapter = ArrayAdapter (this,R.layout.item_sopas,arregloSopas)
         val listView =  findViewById<ListView>(R.id.ListView)
         listView.adapter = adapter
     }
