382de435b6d1097e0a730b1a892107c3d9e860bb
Antonio Osornio <a.osornio@outlook.com>
2020-07-11 15:13:38 -0500

    se implementa por completo los 10 platillos en entradas

--- CocinaYa/app/src/main/java/com/example/cocinaya/IndiceSopas.kt
+++ CocinaYa/app/src/main/java/com/example/cocinaya/IndiceSopas.kt
@@ -17,10 +17,16 @@ class IndiceSopas : AppCompatActivity() {
         setContentView(R.layout.activity_indice_sopas)
 
         val entradas = ArrayList<Entrada>()
-        entradas.add(Entrada("Sopa Aguada", 30.00 , 3.5f,R.drawable.sopaaguada))
+        entradas.add(Entrada("Sopa Aguada", 25.00 , 3.5f,R.drawable.sopaaguada))
         entradas.add(Entrada("Sopa de Tortilla", 30.00, 4.0f,R.drawable.sopadetortilla))
         entradas.add(Entrada("Caldo de Pollo", 50.00, 3.5f,R.drawable.caldodepollo))
         entradas.add(Entrada("Caldo de Res", 50.00, 4.3f,R.drawable.caldoderes))
+        entradas.add(Entrada("Arroz Blanco", 50.00, 4.3f,R.drawable.arrozblanco))
+        entradas.add(Entrada("Arroz Rojo", 25.00 , 3.5f,R.drawable.arrozrojo))
+        entradas.add(Entrada("Arroz Verde", 30.00, 4.0f,R.drawable.arrrozverde))
+        entradas.add(Entrada("Crema de Champiñones", 50.00, 3.5f,R.drawable.cremadechampinones))
+        entradas.add(Entrada("Crema de Queso", 50.00, 4.3f,R.drawable.cremadequeso))
+        entradas.add(Entrada("Espagueti con Crema y Queso", 50.00, 4.3f,R.drawable.espaguetticoncremadequeso))
 
         lista = findViewById(R.id.lista)
         lista?.setHasFixedSize(true)
